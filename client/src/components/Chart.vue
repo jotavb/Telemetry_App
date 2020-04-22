918e13352d0a8d8f767dac7d571db1ee35316b1c
J. Vitor de Brito Camargo <jvbshaders@gmail.com>
2020-04-22 15:33:34 -0700

    added: chart sample wip

---
+++ client/src/components/Chart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Content">
+    <canvas class="chart" ref="canvas" width="600" height="250" style="display: inline"></canvas>
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import Controller from '@/lib/controller'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+    extends: Bar,
+    mounted () {
+       //The data for our dataset
+        let records = this.$store.state.records;
+        let data = {
+            labels: ['Section1', 'Section2', 'Section3'],
+            datasets: [{
+                label: 'Deaths by Section',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [1, 1, 0, 10]
+                }]
+            };
+            // Configuration options go here
+            let options = {};
+        this.renderChart(data, options)
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  max-width: 600px;
+  max-height: 250px;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img class="logo" alt="vfs-logo" src="../assets/vfs_logo.jpg">
     <h1>Welcome to PG17vitor Telemetry App</h1>
+    <chart></chart>
     <records title="Death's History"></records>
   </div>
 </template>
@@ -9,12 +10,16 @@
 <script>
 // @ is an alias to /src
 import Records from '@/components/Records.vue'
+import Chart from '@/components/Chart.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    Records
-  }
+    Records,
+    Chart
+  },
+  //extends: Bar,
 }
 </script>
 
